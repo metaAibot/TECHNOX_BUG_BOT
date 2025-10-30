9af1dc823bde5d26ddcb30b62d3e4a4e7824c94f
GadgetsX <davidagwu210@gmail.com>
2025-10-30 10:23:44 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ async function startBot() {
 
   // ✅ Load Settings
   const settings = typeof loadSettings === 'function' ? loadSettings() : {};
-  let ownerRaw = settings.ownerNumber?.[0] || "2347043204941
+ let ownerRaw = settings.ownerNumber && settings.ownerNumber[0] || "2347043204941";
   ";
   const ownerJid = ownerRaw.includes("@s.whatsapp.net") ? ownerRaw : ownerRaw + "@s.whatsapp.net";
 
